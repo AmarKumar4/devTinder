b836e7a93338e89f6199ff698ee8a056e934e821
Amar <>
2025-01-12 00:15:00 +0530

    DB connected

--- src/multipleRoute.js
+++ src/multipleRoute.js
@@ -16,6 +16,26 @@ app.use("/userData", (req,res,next)=>{
     res.send("Hi ")
 })
 
+
+// Middleware for all routes starting with "/api"
+app.use('/api', (req, res, next) => {
+  console.log('Middleware triggered');
+  next();
+});
+
+app.get('/api/users', (req, res) => {
+  res.send('Users endpoint');
+});
+
+app.get('/api/products', (req, res) => {
+  res.send('Products endpoint');
+});
+
+// Handle all methods for the "/info" route
+app.use('/info', (req, res) => {
+    res.send(`HTTP Method: ${req.method}`);
+  });
+
 app.listen(3000,()=>{
     console.log("port running on 3000")
 })
