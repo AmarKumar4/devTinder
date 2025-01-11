a6090ebc8d9cc6215532a2ad6957b258b26056ea
Amar <>
2025-01-12 01:10:55 +0530

    user model added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
    const express = require('express');
    const app = express();
    const connectDB =require("./config/database")
+   const User  = require("./models/user");
 
+   app.post('/signup', async(req,res)=>{
+
+      const user1 = new User({
+         firstName:"Aakash",
+         lastName:"kumar",
+         emailId:"kumaraakash55@gmail.com",
+         password: "Aakash1234",
+         // age: 32,
+         gender:"Male"
+      });
+   
+      try{
+         await user1.save();
+         res.send("User added successfully!")
+      }catch(err){
+         res.status(400).send("error saving the user"+ err.message)
+      }
+   
+   })
+  
 
    connectDB().then(()=>{
       console.log("Database connection established")
