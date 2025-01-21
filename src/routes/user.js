84b081822e6eca1769750455d05ea0677e96e78d
Amar <>
2025-01-22 00:13:29 +0530

    feed API added

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userAuth } = require('../middleware/userAuth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 const userRouter = express.Router();
 const USER_SAFE_DATA ="firstName lastName age gender photoUrl about skills"
 //get all the pending connection request for the loggedIn user
@@ -54,6 +55,64 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     }
 })
 
-//should not show the accepted , ignored 
+//User should see all the user cards except 
+//1- his own card
+// 2- his connection
+// 3- ignored people
+// 4- already sent the connection request 
 
+userRouter.get("/feed",userAuth, async(req,res)=>{
+    try {
+
+//feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)
+
+//feed?page=2&limit=10 => 11-20 => .skip(10) & .limit(10)
+
+//feed?page=3&limit=10 => 21-30 => .skip(20) & .limit(10)
+
+//feed?page=4&limit=10 => 21-30 => .skip(20) & .limit(10)
+
+//skip = (page-1)*limit;
+const page = req.query.page || 1;
+let limit= req.query.limit || 10;
+limit = limit >50 ? 50: limit;
+const skip = (page-1)*limit;
+        const loggedInUser = req.userProfile;
+        const connectioRequests = await ConnectionRequest.find({
+            $or: [{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}],
+        }).select("fromUserId toUserId status").populate("fromUserId", "firstName").populate("toUserId","firstName")        
+
+        // const userData = await User.find();
+        const hideUsersFromFeed = new Set();
+        //now we will make a set of users to whom we have to avoid and the own also have to avoid 
+        connectioRequests.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId).toString();
+            hideUsersFromFeed.add(req.toUserId).toString();  
+        }); 
+        //now we will filter the User so that it will show only those people user didn't send req or accepted 
+        const freshFeedUser =await User.find({
+            $and: [
+                { _id: {$nin:Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: loggedInUser._id}},
+            ] 
+        }).select(USER_SAFE_DATA).limit(page).skip(skip)
+        res.send(freshFeedUser)
+        // const hideUserFromFeed =  userData.map(async (req)=>{
+        //     // console.log(req._id);
+        //     console.log("Hi " +req._id.toString());
+        //     // const data = await loggedInUserData._id;
+        //     console.log("Dating " +loggedInUserData);
+        //     // console.log(loggedInUserData._id.toString());
+            
+            
+        //     // if(req._id === loggedInUserData.fromUserId._id || req._id === loggedInUserData.toUserId._id )
+        //   return "Hi"
+        // // else return req;
+        // })
+        // // console.log(filterData)
+        // res.send(loggedInUserData);
+    } catch (err) {
+        res.send("Error in feeding"+ err.message);
+    }
+})
 module.exports = userRouter; 
